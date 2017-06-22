0bf47294faab678d6c71885c455a3518a8832f5d
Administrator <271464434@qq.com>
2017-06-22 20:02:26 +0800

    添加工具类

--- mylibrary/src/main/java/com/wangl/mylibrary/util/OkhttpUtil.java
+++ mylibrary/src/main/java/com/wangl/mylibrary/util/OkhttpUtil.java
@@ -1,5 +1,9 @@
 package com.wangl.mylibrary.util;
 
+import android.content.Context;
+
+import com.wangl.mylibrary.R;
+
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -10,19 +14,48 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
- * Created by XANINFO02 on 2016/9/4.
- * 自定义okhttp网络请求
+ * Created by Administrator on 2017/5/15 0015.
+ * 自己对okhttp网络请求进行的进一步处理，
+ * 通过实现Result接口的方法来对网络请求的数据进行进一步处理，
+ * 或者更新UI
  */
-public class OkhttpUtil {
-    private static OkHttpClient okHttpClient = new OkHttpClient();
-    //post请求,返回json数据
-    public static void post(String url, RequestBody body,Callback callback){
-        Request  request = new Request.Builder().url(url).post(body).build();
-        okHttpClient.newCall(request).enqueue(callback);
+
+public  class OkHttpUtil {
+    private static OkHttpClient client = new OkHttpClient();
+//    static Result result;
+    //get请求
+    public static void get(String path, final int code, final Result result, final Context context){
+        Request request = new Request.Builder().url(path).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+               result.result(context.getResources().getString(R.string.error), 0);
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String s = response.body().string();
+                result.result(s,code);
+            }
+        });
     }
-    //get请求,返回json数据
-    public static void get(String url,Callback callback){
-        Request request = new Request.Builder().url(url).build();
-        okHttpClient.newCall(request).enqueue(callback);
+    //需要实现的接口,解析数据更新UI等操作,该方法是运行在子线程中
+    public interface Result{
+        void result(String s, int code);
+    }
+    //post请求
+    public static void post(String path, final int code, RequestBody body, final Result result, final Context context){
+        Request request = new Request.Builder().url(path).post(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                result.result(context.getResources().getString(R.string.error),0);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String s = response.body().string();
+                result.result(s,code);
+            }
+        });
     }
 }
